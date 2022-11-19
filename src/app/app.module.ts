e3334a82f868a442e7c5a1030edde8ef64ed7ced
Matheus-Reinert <reinert.matheus@hotmail.com>
2022-11-19 17:37:10 -0300

    tela de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {HttpClientModule} from '@angular/common/http'
+
+
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    FontAwesomeModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/resources/services/login.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { RequestLogin } from 'src/app/resources/models/RequestLogin';
+import { Injectable } from '@angular/core';
+import { ResponseLogin } from '../models/ResponseLogin';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  public doLogin(requestLogin:RequestLogin): Observable<ResponseLogin>{
+    return this.httpClient.post<ResponseLogin>(
+      'http://localhost:8080/login',
+       requestLogin);
+  }
+}
